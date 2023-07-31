7b144c8073171cf53fd29dd1d20d94f4e604b469
Denis Cooper <denis.cooper09@gmail.com>
2023-07-31 19:22:06 +0100

    Project Initialization
    - Create basic directories structure for the project.
    - Create .gitignore file to ignore CLion files.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ *  ╔═╗╦ ╦╔╗ ╔═╗╦═╗  ╦  ╦╔═╗╔═╗  ╦  ╔═╗╔╗ ╔═╗╦═╗╔═╗╔╦╗╔═╗╦═╗╦ ╦
+ *  ║  ╚╦╝╠╩╗║╣ ╠╦╝  ║  ║╠╣ ║╣   ║  ╠═╣╠╩╗║ ║╠╦╝╠═╣ ║ ║ ║╠╦╝╚╦╝
+ *  ╚═╝ ╩ ╚═╝╚═╝╩╚═  ╩═╝╩╚  ╚═╝  ╩═╝╩ ╩╚═╝╚═╝╩╚═╩ ╩ ╩ ╚═╝╩╚═ ╩
+ *
+ *  main.cpp
+ *  Entry point of the application.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
